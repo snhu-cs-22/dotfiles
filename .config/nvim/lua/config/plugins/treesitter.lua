22b0d31c8df33dcc076d18fbfac96bd2e220dcf5
snhu-cs-22 <110997198+snhu-cs-22@users.noreply.github.com>
2026-03-18 13:08:13 -0400

    feat(nvim): add treesitter incremental selection

--- .config/nvim/lua/config/plugins/treesitter.lua
+++ .config/nvim/lua/config/plugins/treesitter.lua
@@ -16,6 +16,15 @@ require('nvim-treesitter.configs').setup {
     enable = true,
     additional_vim_regex_highlighting = false,
   },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn", -- set to `false` to disable one of the mappings
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
 }
 
 vim.api.nvim_create_autocmd('FileType', {
