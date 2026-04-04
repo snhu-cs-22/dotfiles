20c4e05fab00daf715c670652239828c88614049
snhu-cs-22 <110997198+snhu-cs-22@users.noreply.github.com>
2026-04-03 22:14:09 -0400

    feat(nvim): add IME engine selector to ime_enable

--- .config/nvim/plugin/ime_enable.lua
+++ .config/nvim/plugin/ime_enable.lua
@@ -1,26 +1,41 @@
 -- Source: https://qiita.com/keith_campbell/items/6787dca658a3c9460bca
 
-local function GetFcitxStatus()
-    local handle = io.popen('fcitx-remote')
-    local result = handle:read('*a')
-    handle:close()
-    return result == '2\n'
+local ime_commands = nil;
+if vim.fn.executable('fcitx5-remote') then
+	ime_commands = {
+		status = 'fcitx5-remote',
+		disable = 'fcitx5-remote -c',
+		enable = 'fcitx5-remote -o',
+	}
+elseif vim.fn.executable('fcitx-remote') then
+	ime_commands = {
+		status = 'fcitx-remote',
+		disable = 'fcitx-remote -c',
+		enable = 'fcitx-remote -o',
+	}
 end
 
-local function Disable()
-    -- Save fcitx status when leaving insert mode
-    vim.g.fcitx_status = GetFcitxStatus()
-    -- Disable (Japanese) input method
-    os.execute('fcitx-remote -c')
-end
+if ime_commands ~= nil then
+	local function GetFcitxStatus()
+		local handle = io.popen(ime_commands['status'])
+		local result = handle:read('*a')
+		handle:close()
+		return result == '2\n'
+	end
 
-local function Enable(status)
-    if status then
-        os.execute('fcitx-remote -o')
-    end
-end
+	local function Disable()
+		-- Save fcitx status when leaving insert mode
+		vim.g.fcitx_status = GetFcitxStatus()
+		-- Disable (Japanese) input method
+		os.execute(ime_commands['disable'])
+	end
 
-if vim.fn.executable('fcitx-remote') then
+	local function Enable(status)
+		if status then
+			os.execute(ime_commands['enable'])
+		end
+	end
+
 	vim.g.fcitx_status = GetFcitxStatus()
 
 	-- Autocmd for InsertLeave and InsertEnter
